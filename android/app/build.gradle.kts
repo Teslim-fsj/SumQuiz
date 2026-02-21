9c68bf12173cfd4210809b7a3c9666f2b8eae225
Teslim-fsj <oladejoteslim84@gmail.com>
2026-02-21 17:26:46 +0100

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -35,8 +35,8 @@ android {
         applicationId = "com.sumquiz.app"
         minSdk = 26
         targetSdk = 35  // ← Updated to 35 (safe & recommended)
-        versionCode = 17
-        versionName = "1.1.8"
+        versionCode = 20
+        versionName = "1.1.10"
         multiDexEnabled = true
     }
 
