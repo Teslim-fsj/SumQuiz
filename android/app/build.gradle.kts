bfe44c3ea4e442ff1bc2a6509d76c76059345704
Teslim-fsj <oladejoteslim84@gmail.com>
2026-03-11 11:39:08 +0100

    Update version code and version name in build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -36,8 +36,8 @@ android {
         applicationId = "com.sumquiz.app"
         minSdk = 26
         targetSdk = 35  // ← Updated to 35 (safe & recommended)
-        versionCode = 33
-        versionName = "1.1.23"
+        versionCode = 34
+        versionName = "1.2.1"
         multiDexEnabled = true
     }
 
