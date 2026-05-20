8548e690392e8adbd31fdaa66c3e5e1b4e7a17b3
Teslim Oladejo <oladejoteslim32@gmail.com>
2026-05-20 15:42:15 +0100

    Fix CI build: Downgrade app_links to ^6.4.1 to avoid Dart SDK version 3.9.0 mismatch under Flutter 3.35.0

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -35,7 +35,6 @@ import share_plus
 import shared_preferences_foundation
 import speech_to_text
 import url_launcher_macos
-import video_player_avfoundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AppLinksMacosPlugin.register(with: registry.registrar(forPlugin: "AppLinksMacosPlugin"))
@@ -68,5 +67,4 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SpeechToTextPlugin.register(with: registry.registrar(forPlugin: "SpeechToTextPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
-  FVPVideoPlayerPlugin.register(with: registry.registrar(forPlugin: "FVPVideoPlayerPlugin"))
 }
